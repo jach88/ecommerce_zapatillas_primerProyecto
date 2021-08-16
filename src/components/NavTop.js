c628c29239022a38106a5722c7c5b710d476b683
jach88 <33399125+jach88@users.noreply.github.com>
2021-08-16 00:15:31 -0500

    arreglado de navbar y creacion vista carrito

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -1,4 +1,5 @@
 import { Navbar, Container, Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 import Logo from "../assets/Logo_tienda2.jpg"
 import Fondo from "../assets/inicio.jpg"
@@ -12,9 +13,22 @@ export default function NavTop() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link to="/">Inicio</Nav.Link>
-                        <Nav.Link href="#link">Login</Nav.Link>
+                    <Nav className="me-auto" >
+                        <Nav.Link>
+                            <Link to="/" style={{color:"white", textDecoration:"None"}}>
+                                Inicio
+                            </Link> 
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/Login" style={{color:"white", textDecoration:"None"}}>
+                                Login
+                            </Link> 
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/Carrito" style={{color:"white", textDecoration:"None"}}>
+                                Carrito
+                            </Link> 
+                        </Nav.Link>
                         
                     </Nav>
                 </Navbar.Collapse>
