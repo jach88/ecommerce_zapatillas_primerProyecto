f2bfdacc57a1da42baf3cac2920e2c4d27bc6bdf
jach88 <33399125+jach88@users.noreply.github.com>
2021-08-16 00:47:41 -0500

    vista_carrito_implementada
    vista_carrito_implementada

--- src/views/CarritoView.js
+++ src/views/CarritoView.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useContext } from "react"
+import { CarritoContext } from "../context/carritoContext"
 
 export default function CarritoView() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+        const { carrito } = useContext(CarritoContext)
+
+
+        return (
+            <div className="container">
+                <div className="my-4 text-center">
+                    <h1 className="fw-bold">
+                        <i className="fas fa-shopping-cart me-3" />
+                        Carrito de Compras
+                    </h1>
+                </div>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Precio Unitario</th>
+                            <th>Precio Total</th>                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {carrito.map((prod, i) => (
+                            <tr key={i}>
+                                <td>{prod.nombre}</td>
+                                <td></td>
+                                <td>{prod.precio}</td>
+                                <td></td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                </table>
+
+            </div>
+        )
 }
